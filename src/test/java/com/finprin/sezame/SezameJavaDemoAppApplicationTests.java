799eb02f1e5f5e50fa3654bd989ef76b93527133
Jonathan Gutschi <jonathan@gutschi.net>
2016-03-14 12:56:09 +0100

    Initial commit. Java demo application for Sezame Java SDK.

---
+++ src/test/java/com/finprin/sezame/SezameJavaDemoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.finprin.sezame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SezameJavaDemoAppApplication.class)
+public class SezameJavaDemoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
